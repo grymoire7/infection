cba158692dc54b35b8739854ea4ce60eede1d4ac
Tracy Atteberry <tracy@magicbydesign.com>
2025-09-11 19:13:35 -0500

    refactor: improve logo tween management and dot animation handling

--- src/game/scenes/Splash.ts
+++ src/game/scenes/Splash.ts
@@ -5,9 +5,7 @@ import { EventBus } from '../EventBus';
 export class Splash extends Scene
 {
     background: GameObjects.Image;
-    logo: GameObjects.Image;
     title: GameObjects.Text;
-    logoTween: Phaser.Tweens.Tween | null;
     animatedDots: any[] = [];
     dotTweens: Phaser.Tweens.Tween[] = [];
 
@@ -45,14 +43,7 @@ export class Splash extends Scene
     
     changeScene ()
     {
-        if (this.logoTween)
-        {
-            this.logoTween.stop();
-            this.logoTween = null;
-        }
-
         this.stopAnimatedDots();
-
         this.scene.start('Game');
     }
 
@@ -134,37 +125,4 @@ export class Splash extends Scene
         this.animatedDots = [];
     }
 
-    moveLogo (vueCallback: ({ x, y }: { x: number, y: number }) => void)
-    {
-        if (this.logoTween)
-        {
-            if (this.logoTween.isPlaying())
-            {
-                this.logoTween.pause();
-            }
-            else
-            {
-                this.logoTween.play();
-            }
-        } 
-        else
-        {
-            this.logoTween = this.tweens.add({
-                targets: this.logo,
-                x: { value: 750, duration: 3000, ease: 'Back.easeInOut' },
-                y: { value: 80, duration: 1500, ease: 'Sine.easeOut' },
-                yoyo: true,
-                repeat: -1,
-                onUpdate: () => {
-                    if (vueCallback)
-                    {
-                        vueCallback({
-                            x: Math.floor(this.logo.x),
-                            y: Math.floor(this.logo.y)
-                        });
-                    }
-                }
-            });
-        }
-    }
 }
